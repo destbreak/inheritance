ffaca4e4ee75324c3103cc580f9b725c644d3aa5
Reynaldi Hadiansyah <rey.hadiansyah7th@gmail.com>
2020-04-11 20:39:26 +0700

    Add - Class Student.cs

---
+++ Students.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class Student : Person
+    {
+        public string StudentId { get; set; }
+        public string Email { get; set; }
+    }
+}
