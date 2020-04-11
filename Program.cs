f8e41ceb6597ea400ac1375b5cb4a1ae8a0d1de9
Reynaldi Hadiansyah <rey.hadiansyah7th@gmail.com>
2020-04-11 20:43:28 +0700

    Add - Variable in 'Program.cs'

--- Program.cs
+++ Program.cs
@@ -6,5 +6,24 @@ using System.Threading.Tasks;
 
 namespace inheritance
 {
-    
+    Person person = new Person();
+    person.Name = "Mike";
+    person.Age = 20;
+    person.GetNameAndAge();
+
+    Teacher teacher = new Teacher();
+    teacher.Name = "Thomas";
+    teacher.Age = 35;
+    teacher.TeacherId = "190302";
+    teacher.Subject = "Math";
+    teacher.GetNameAndAge();
+
+    Student student = new Student();
+    student.Name = "Sara";
+    student.Age = 19;
+    student.StudentId = "10506";
+    student.Email = "sara1234@amikom.ac.id";
+    student.GetNameAndAge();
+
+    Console.ReadKey();
 }
