6162beb828d032b772b9140f03fc2297510e8612
Reynaldi Hadiansyah <rey.hadiansyah7th@gmail.com>
2020-04-11 20:36:03 +0700

    Add - Class Person.cs

---
+++ Person.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The person has name : {0} and age : {1}", Name, Age);
+        }
+    }
+}
