ea482fbbfd794ff31028b0914ddb24f2342e6b51
Reynaldi Hadiansyah <rey.hadiansyah7th@gmail.com>
2020-04-11 21:10:05 +0700

    Fix - Structure and Error

--- Program.cs
+++ Program.cs
@@ -1,29 +1,35 @@
 using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace inheritance
 {
-    Person person = new Person();
-    person.Name = "Mike";
-    person.Age = 20;
-    person.GetNameAndAge();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Mike";
+            person.Age = 20;
+            person.GetNameAndAge();
 
-    Teacher teacher = new Teacher();
-    teacher.Name = "Thomas";
-    teacher.Age = 35;
-    teacher.TeacherId = "190302";
-    teacher.Subject = "Math";
-    teacher.GetNameAndAge();
+            Teacher teacher = new Teacher();
+            teacher.Name = "Thomas";
+            teacher.Age = 35;
+            teacher.TeacherId = "190302";
+            teacher.Subject = "Math";
+            teacher.GetNameAndAge();
 
-    Student student = new Student();
-    student.Name = "Sara";
-    student.Age = 19;
-    student.StudentId = "10506";
-    student.Email = "sara1234@amikom.ac.id";
-    student.GetNameAndAge();
+            Student student = new Student();
+            student.Name = "Sara";
+            student.Age = 19;
+            student.StudentId = "10506";
+            student.Email = "sara1234@amikom.ac.id";
+            student.GetNameAndAge();
 
-    Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
 }
