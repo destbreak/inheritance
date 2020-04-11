406f28c3046685c30043e2341ca6c8417473aa5f
Reynaldi Hadiansyah <rey.hadiansyah7th@gmail.com>
2020-04-11 20:37:51 +0700

    Add - Class Teacher.cs

---
+++ Teacher.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class Teacher : Person
+    {
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+    }
+}
